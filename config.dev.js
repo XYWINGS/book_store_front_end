6ae451bd9aa64873be03e82e8229c4b07e55f492
JIshanRandika <jishanrandika@gmail.com>
2024-02-13 07:21:45 +0530

    update client id

---
+++ config.dev.js
@@ -0,0 +1,21 @@
+window.config = {
+    bookStoreApiUrl:
+    "https://04ef0bb4-e44c-469a-b881-d5e935130fb2-dev.e1-us-east-azure.choreoapis.dev/kqrg/bookstorebackend/books-2e9/v1",
+    auth: {
+        signInRedirectURL:
+        "https://5debbc07-e501-49bf-b27f-769ba7d122e7.e1-us-east-azure.choreoapps.dev",
+        signOutRedirectURL:
+        "https://5debbc07-e501-49bf-b27f-769ba7d122e7.e1-us-east-azure.choreoapps.dev",
+        clientID:
+        "MfYnsn0Meezx2ishrd3PI39ju8Ea",
+        baseUrl: "https://api.asgardeo.io/t/iorg",
+     }
+}
+
+{
+    "clientID": "Ti8X5L5GvvDMg1qTKcZb1i9LVx8a",
+    "baseUrl": "https://api.asgardeo.io/t/iorg",
+    "signInRedirectURL": "https://5debbc07-e501-49bf-b27f-769ba7d122e7.e1-us-east-azure.choreoapps.dev",
+    "signOutRedirectURL": "https://5debbc07-e501-49bf-b27f-769ba7d122e7.e1-us-east-azure.choreoapps.dev",
+    "scope": ["profile"]
+}
